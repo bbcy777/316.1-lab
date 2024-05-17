d7c8d9e1337375eb88c13f6e76087078675afc30
JinZ <bbcy777@gmail.com>
2024-05-17 15:52:20 -0700

    316.3.1

--- index.mjs
+++ index.mjs
@@ -1,10 +1,21 @@
 //Menu data structure
 var menuLinks = [
-    { text: 'about', href: '/about'},
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },  
-];
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
+  ];
 
 //Part 1
 let mainEl = document.getElementsByTagName(`main`);
@@ -15,7 +26,7 @@ mainEl[0].classList.toggle('flex-ctr');
 //Part 2
 let topMenuEl = document.getElementById(`top-menu`);
 // console.log(topMenuEl);
-topMenuEl.style.height = "100%"
+topMenuEl.style.height = "100%";
 topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 topMenuEl.classList.toggle("flex-around");
 
@@ -27,9 +38,24 @@ for (let i = 0; i < menuLinks.length; i++){
     link.textContent = menuLinks[i].text;
     topMenuEl.appendChild(link);
 }
-// menuLinks.forEach((link) => {
-//     let newLink = document.createElement(`a`);
-//     newLink.setAttribute(`href`, link.href);
-//     newLink.textContent = link.text;
-//     topMenuEl.appendChild(newLink);
-// })
+
+
+//316.3.1 Part 3
+const subMenuEl = document.getElementById(`sub-menu`);
+subMenuEl.style.height = `100%`;
+subMenuEl.style.backgroundColor = `var(--sub-menu-bg)`;
+subMenuEl.classList.toggle(`flex-around`);
+subMenuEl.style.position = `absolute`;
+subMenuEl.style.top = `0`;
+
+//part 4 Adding Menu Interaction
+let topMenuLinks = document.querySelectorAll('a');
+console.log(topMenuLinks);
+
+topMenuEl.addEventListener(`click`, (event)=>{
+    event.preventDefault();
+    console.log(event);
+    console.log(event.target);
+    if(event.target != `a`) return;
+    event.target.classList.toggle(`active`)
+})
